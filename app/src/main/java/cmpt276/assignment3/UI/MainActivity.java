6e21f0fa7b01bef98fc398b3ddeb773e61960589
seshghie <seshghie@sfu.ca>
2020-06-16 21:45:42 -0700

    Welcome screen complete.
    Welcome screen is complete and fully functional, just needs intent to launch main menu after welcome screen expires/when screen is clicked (which will be implemented as part of main menu development).

---
+++ app/src/main/java/cmpt276/assignment3/UI/MainActivity.java
@@ -0,0 +1,78 @@
+package cmpt276.assignment3.UI;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import cmpt276.assignment3.R;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    //INITIALIZE VARIABLES
+    RelativeLayout ws_Layout;
+    TextView tv_Skip;
+    TextView tv_Title1;
+    TextView tv_Title2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //DEFINE VARIABLES
+        ws_Layout = findViewById(R.id.ws_Layout);
+        tv_Skip = findViewById(R.id.tv_ws_Skip);
+        tv_Title1 = findViewById(R.id.tv_ws_Title1);
+        tv_Title2 = findViewById(R.id.tv_ws_Title2);
+
+
+        startAnimations();
+        Handler freezeTime = new Handler();
+        freezeTime.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                //intent to run main menu
+            }
+        }, 5000);
+
+
+        ws_Layout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //intent to run main menu
+            }
+
+        });
+
+    }
+
+    private void startAnimations()
+    {
+
+        Animation anim_Title1 = AnimationUtils.loadAnimation(this, R.anim.ws_title1_anim);
+        Animation anim_Title2 = AnimationUtils.loadAnimation(this, R.anim.ws_title2_anim);
+        Animation anim_Skip = AnimationUtils.loadAnimation(this, R.anim.ws_skip_anim);
+
+        ws_Layout.setBackgroundResource(R.drawable.bg_menu);
+        tv_Skip.startAnimation(anim_Skip);
+        tv_Title1.startAnimation(anim_Title1);
+        tv_Title2.startAnimation(anim_Title2);
+
+    }
+
+
+
+}
--- app/src/main/java/cmpt276/assignment3/UI/SecondFragment.java
+++ app/src/main/java/cmpt276/assignment3/UI/SecondFragment.java
@@ -1,4 +1,4 @@
-package cmpt276.assignment3;
+package cmpt276.assignment3.UI;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import cmpt276.assignment3.R;
 
 public class SecondFragment extends Fragment {
 
@@ -22,13 +24,6 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
     }
+
 }
