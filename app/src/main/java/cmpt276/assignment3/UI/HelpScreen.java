b005bfdcdc45c4c8c9e41a5eb5e61b8cade8f6f7
seshghie <seshghie@sfu.ca>
2020-06-17 00:29:05 -0700

    Welcome screen design change.
    Changed the design of the welcome screen, added activity and class for the Help screen.

---
+++ app/src/main/java/cmpt276/assignment3/UI/HelpScreen.java
@@ -0,0 +1,22 @@
+package cmpt276.assignment3.UI;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import cmpt276.assignment3.R;
+
+public class HelpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_screen);
+    }
+}
---
+++ app/src/main/java/cmpt276/assignment3/UI/Second2Fragment.java
@@ -0,0 +1,28 @@
+package cmpt276.assignment3.UI;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import cmpt276.assignment3.R;
+
+public class Second2Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second2, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
